cf0bd64394ae647fbaeed9f1e09134f5244c2f70
Ulises Castorena <ulises.castorena@mx.bosch.com>
2022-05-03 00:21:29 +0200

    Add navigation

--- app/src/main/java/com/architechcoders/weather/ui/activities/MainActivity.kt
+++ app/src/main/java/com/architechcoders/weather/ui/activities/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.architechcoders.weather
+package com.architechcoders.weather.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.architechcoders.weather.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
