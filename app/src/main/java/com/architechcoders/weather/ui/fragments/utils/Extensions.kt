409850c856cda48d6b7b24165f2bb03b9052e2f4
Ulises Castorena <ulises.castorena@mx.bosch.com>
2022-05-28 13:22:47 +0200

    Add extension for distinct collect

--- app/src/main/java/com/architechcoders/weather/ui/fragments/utils/Extensions.kt
+++ app/src/main/java/com/architechcoders/weather/ui/fragments/utils/Extensions.kt
@@ -7,8 +7,15 @@ import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.DiffUtil
 import coil.load
+import com.architechcoders.domain.Weather
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
+import java.time.Duration
+import java.time.Instant
+import java.util.concurrent.TimeUnit
 
 inline fun <T> basicDiffUtil(
     crossinline areItemsTheSame: (T, T) -> Boolean = { old, new -> old == new },
@@ -33,4 +40,24 @@ fun LifecycleOwner.launchOnLifecycle(
             action()
         }
     }
+}
+
+fun <T, R> LifecycleOwner.distinctCollect(
+    flow: Flow<T>,
+    transform: (T) -> R,
+    state: Lifecycle.State = Lifecycle.State.STARTED,
+    result: (R) -> Unit
+) {
+    lifecycleScope.launch {
+        repeatOnLifecycle(state) {
+            flow.map { transform(it) }.distinctUntilChanged().collect(result)
+        }
+    }
+}
+
+fun Weather.shouldUpdate(minMinutes: Int = 30): Boolean {
+    val time = Instant.ofEpochMilli(timeRequested)
+    val currentTime = Instant.now()
+    val dif = Duration.between(time, currentTime)
+    return TimeUnit.MINUTES.convert(dif.seconds, TimeUnit.SECONDS) >= minMinutes
 }
